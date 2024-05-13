ba6dba1463dcb340d3e0afb31629a74d7507328f
Abdulaziz2888 <164225391+Abdulaziz2888@users.noreply.github.com>
2024-05-13 22:10:22 +0600

    Update hierarchy3.java

--- src/hierarchy3.java
+++ src/hierarchy3.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class hierarchy3 {
+final class Manager {
+    private Employee employee;
+    private String department;
+
+    public Manager(Employee employee, String department){
+        this.employee = employee;
+        this.department = department;
+    }
+
+    public String getName(){
+        return employee.getName();
+    }
+
+    public int getAge(){
+        return employee.getAge();
+    }
+
+    public Address getAddress(){
+        return employee.getAddress();
+    }
+
+    public String getEmployeeId(){
+        return employee.getEmployeeId();
+    }
+
+    public String getDepartment(){
+        return department;
+    }
+
+    public void performTask(String task){
+        System.out.println("As a manager, delegating task: " + task);
+    }
 }
