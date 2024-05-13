2e6bd86d175b629fb1673ca3f71ce6b6c1b4a32f
Abdulaziz2888 <164225391+Abdulaziz2888@users.noreply.github.com>
2024-05-13 22:09:49 +0600

    Update Enum.java

--- src/Enum.java
+++ src/Enum.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Enum {
+enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
+
+class Address {
+    private String street;
+    private String city;
+    private String country;
+
+    public Address(String street, String city, String country) {
+        this.street = street;
+        this.city = city;
+        this.country = country;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
 }
