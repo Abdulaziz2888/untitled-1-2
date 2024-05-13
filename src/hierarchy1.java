4662b628101f411d8553a58da560612fdbe052f6
Abdulaziz2888 <164225391+Abdulaziz2888@users.noreply.github.com>
2024-05-13 22:10:45 +0600

    Update hierarchy1.java

--- src/hierarchy1.java
+++ src/hierarchy1.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class hierarchy1 {
+class Person {
+    private String name;
+    private int age;
+    private Address address;
+
+    public Person(String name, int age, Address address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
 }
