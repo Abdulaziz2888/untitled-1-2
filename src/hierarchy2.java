257eb3c594f01c7bb016fa5050d01787c181e82a
Abdulaziz2888 <164225391+Abdulaziz2888@users.noreply.github.com>
2024-05-13 22:11:09 +0600

    Update hierarchy2.java

--- src/hierarchy2.java
+++ src/hierarchy2.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class hierarchy2 {
+class Employee extends Person {
+    private String employeeId;
+
+    public Employee(String name, int age, Address address, String employeeId) {
+        super(name, age, address);
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+
+    public void work() {
+        System.out.println("Employee is working...");
+    }
+
+    public void work(String task) {
+        System.out.println("Employee is working on: " + task);
+    }
 }
