7f5e8d3db729190a2d74202d0d216238cf5516fa
Abdulaziz2888 <164225391+Abdulaziz2888@users.noreply.github.com>
2024-05-13 22:09:08 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Address address = new Address("123 Main St", "Anytown", "Country");
+        Employee objectA = new Employee("John", 30, address, "E001");
+        Manager objectB = new Manager(objectA, "HR");
+        Manager objectC = new Manager(new Employee("Alice", 35, address, "E002"), "Finance");
+
+        // Вывод свойств объекта objectA
+        System.out.println("Employee:");
+        System.out.println("Name: " + objectA.getName());
+        System.out.println("Age: " + objectA.getAge());
+        System.out.println("Address: " + objectA.getAddress().getStreet() + ", " + objectA.getAddress().getCity() + ", " + objectA.getAddress().getCountry());
+        System.out.println("Employee ID: " + objectA.getEmployeeId());
+
+        objectA.work();
+        objectA.work("Project presentation");
+
+        System.out.println();
+        System.out.println("Manager:");
+        System.out.println("Name: " + objectB.getName());
+        System.out.println("Age: " + objectB.getAge());
+        System.out.println("Address: " + objectB.getAddress().getStreet() + ", " + objectB.getAddress().getCity() + ", " + objectB.getAddress().getCountry());
+        System.out.println("Employee ID: " + objectB.getEmployeeId());
+        System.out.println("Department: " + objectB.getDepartment());
+
+        objectB.work();
+        objectB.work("Hiring process");
+
+        System.out.println();
+
+        System.out.println("Manager:");
+        System.out.println("Name: " + objectC.getName());
+        System.out.println("Age: " + objectC.getAge());
+        System.out.println("Address: " + objectC.getAddress().getStreet() + ", " + objectC.getAddress().getCity() + ", " + objectC.getAddress().getCountry());
+        System.out.println("Employee ID: " + objectC.getEmployeeId());
+        System.out.println("Department: " + objectC.getDepartment());
+
+        objectC.work();
+        objectC.work("Budget planning");
     }
 }
